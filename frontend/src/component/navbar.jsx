47c21413cb46c69982766e998856d59b4e906d6c
muhammadharoun <hro19502001@gmail.com>
2021-12-12 00:06:32 +0200

    nav bar

---
+++ frontend/src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import { PageHeader } from 'antd';
+
+
+const Navbar = () => (
+
+    <PageHeader
+    className="site-page-header"
+    title="TO DO APP"
+    avatar={{ src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4' }}
+    />
+);
+
+
+export default Navbar;
